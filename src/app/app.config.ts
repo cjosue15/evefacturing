0cbf756848fcaac7d1c5d9c19a4d13c45f2ec1ca
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-09-07 17:49:16 -0500

    feat(guards) apply into routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
 import { MATERIAL_OPTIONS } from './material.config';
 
 console.log(MATERIAL_OPTIONS);
 
 export const appConfig: ApplicationConfig = {
-  providers: [MATERIAL_OPTIONS, provideRouter(routes), provideAnimations()],
+  providers: [
+    MATERIAL_OPTIONS,
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
+import { authGuard, publicGuard } from '@core/guards/auth.guard';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    canActivate: [publicGuard],
+    children: [
+      {
+        path: 'log-in',
+        loadComponent: () => import('@pages/auth/login/login.component'),
+      },
+    ],
+  },
+  {
     path: '',
-    loadComponent: () => import('@pages/home/home.component'),
+    canActivate: [authGuard],
+    loadComponent: () => import('@core/components/layout/layout.component'),
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('@pages/home/home.routes').then((r) => r.HOME_ROUTES),
+      },
+    ],
   },
 ];
